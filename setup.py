f91e4aa5b5d80b116dcf20f74b23c173b426719a
Parshva Runwal <runwalparshva3@gmail.com>
2022-10-21 16:34:29 +0530

    Initial setup.py for testing the build

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="spark",
+    version="0.0.1",
+    description="Python SDK for Newron Model Logging",
+    url="https://www.newron.ai",
+    author="Parshva Runwal",
+    author_email="parshva@newron.ai",
+    license="MIT",
+    keywords="",
+    packages=find_packages(),
+)
