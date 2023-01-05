46decb4c84f1052369020875f14f413bde68ead8
Parshva Runwal <runwalparshva3@gmail.com>
2023-01-05 21:30:38 +0530

    Updated feedbacks structure and added application_env field

--- spark/common/types/Record.py
+++ spark/common/types/Record.py
@@ -1,21 +1,22 @@
 from dataclasses import dataclass, field
 from typing import Dict, List, Literal, Optional, TypedDict
 
-from spark.common.utils import get_current_timestamp
+from common.utils import get_current_timestamp
 
 
-class Prediction(TypedDict):
+class DataRow(TypedDict):
     value: str
-    type: Literal["string", "number", "categorical", "timestamp", "image"]
+    type: Literal["string", "number", "categorical", "timestamp", "url"]
 
 
 @dataclass
 class Record:
-    inputs: Optional[Dict[str, Prediction]] = field(default_factory=dict)
-    outputs: Optional[Dict[str, Prediction]] = field(default_factory=dict)
+    inputs: Optional[Dict[str, DataRow]] = field(default_factory=dict)
+    outputs: Optional[Dict[str, DataRow]] = field(default_factory=dict)
     feedback_keys: Optional[List[str]] = field(default_factory=list)
     feedback_id: Optional[str] = field(default_factory=lambda: None)
-    feedbacks: Optional[Dict[str, str]] = field(default_factory=dict)
+    feedbacks: Optional[Dict[str, DataRow]] = field(default_factory=dict)
     ignore_inputs: Optional[List[str]] = field(default_factory=list)
     timestamp: Optional[int] = field(default_factory=get_current_timestamp)
     version: Optional[str] = field(default_factory=lambda: "1")
+    application_env: Optional[str] = field(default_factory=lambda: "dev")
